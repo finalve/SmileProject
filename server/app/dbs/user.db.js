83b437e469fdd47b62c02a1e0d54e8691350847b
waleesin <veewutsing@gmail.com>
2023-01-11 20:32:57 +0700

    Update user.db.js

--- server/app/dbs/user.db.js
+++ server/app/dbs/user.db.js
@@ -3,20 +3,34 @@ const { Spot } = require('@binance/connector');
 class USERS {
 	#client;
 	#pwd;
+	#wsRef;
 	constructor({ label, pwd, key, serect }) {
 		this.label = label;
 		this.key = key;
 		this.#pwd = pwd;
 		this.#client = new Spot(key, serect);
+		this.#report();
 	}
 
-	async arbitrage({ data}) {
-		const response = await this.#newOrder(data[1].symbol, 'BUY', data[1].quantity, data[1].price)
-		console.log(response);
+	async arbitrage({ data }) {
+		//const response = await this.#newOrder(data[1].symbol, 'BUY', data[1].quantity, data[1].price)
+		//response = await this.#newOrder(data[2].symbol, 'SELL', data[2].quantity, data[2].price)
+		//response = await this.#newOrder(data[3].symbol, 'SELL', data[2].quantity, data[3].price)
+		//console.log(response.data);
 	}
+
+	async #report() {
+		const ListenKey = await this.#createListenKey();
+		this.#wsRef = this.#client.userData(ListenKey.listenKey, {
+			open: () => console.debug(`Connected to Socket Report ${this.label}`),
+			close: () => console.debug(`Disconnected with Socket Report ${this.label}`),
+			message: data => { console.log(JSON.parse(data)) }
+		});
+	}
+
 	async #newOrder(symbol, process, quantity, price) {
 		try {
-			const response = await this.#client.newOrder(symbol, process,'LIMIT',
+			const response = await this.#client.newOrder(symbol, process, 'LIMIT',
 				{
 					quantity: quantity,
 					price: price,
@@ -26,6 +40,10 @@ class USERS {
 		} catch (error) {
 			return error.response;
 		}
+	}
+	async #createListenKey() {
+		const response = await this.#client.createListenKey()
+		return response.data;
 	}
 
 	async #cancelOrder(symbol, orderId) {
