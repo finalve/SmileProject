b1cbe7ed1cf417c7c2dae6a5d42b21c6b8ccff98
waleesin <veewutsing@gmail.com>
2023-01-12 16:20:13 +0700

    Update user.db.js

--- server/app/dbs/user.db.js
+++ server/app/dbs/user.db.js
@@ -14,10 +14,10 @@ class USERS {
 	}
 
 	async arbitrage({ data }) {
-		//const response = await this.#newOrder(data[1].symbol, 'BUY', data[1].quantity, data[1].price)
+		
 		if (this.#openOrder.length < 1) {
-			const response = await this.#newOrder('BTCUSDT', 'BUY', 0.0009, '15000')
-			response.data.step = 1;
+			//const response = await this.#newOrder('BTCUSDT', 'BUY', 0.0009, '15000')
+			const response = await this.#newOrder(data[1].symbol, 'BUY', data[1].quantity, data[1].price)
 			this.#openOrder.push({ data: data, response: response.data });
 		}
 
@@ -46,19 +46,37 @@ class USERS {
 						currentOrder: response.X,
 						orderId: response.i
 					};
-					const order = this.#openOrder.find(x => x.response.orderId == json.orderId);
-					if (order) {
-						switch(json.currentOrder) {
-							case "NEW":
-								this.#log(`Created Order ${json.orderId} Symbol ${json.symbol}`);
-							  break;
-							case "CANCELED":
-								this.#openOrder = this.#openOrder.filter(x => x.response.orderId !== json.orderId);
-								this.#log(`Canceled Order ${json.orderId} Symbol ${json.symbol}  Length ${this.#openOrder.length}`);
-							  break;
-							default:
-								console.log(order.data);
-						  }
+
+					switch (json.currentOrder) {
+						case "NEW":
+							this.#log(`Created Order ${json.orderId} Symbol ${json.symbol}`);
+							break;
+						case "CANCELED":
+							this.#openOrder = this.#openOrder.filter(x => x.response.orderId !== json.orderId);
+							this.#log(`Canceled Order ${json.orderId} Symbol ${json.symbol}`);
+							break;
+						case "FILLED":
+							this.#log(`Filled Order ${json.orderId} Symbol ${json.symbol}`);
+							const order = this.#openOrder.find(x => x.response.orderId == json.orderId);
+							if (order) {
+								if(order.response.symbol == 'BTCUSDT')
+								{
+									this.#openOrder = this.#openOrder.filter(x => x.response.orderId !== json.orderId);
+									this.#log(`Arbitrage Success Symbol [${order.data[1].symbol} ${order.data[2].symbol} ${order.data[3].symbol}]`);
+								}
+								else if (order.response.symbol.includes('USDT')) {
+									const response = await this.#newOrder(order.data[2].symbol, 'SELL', order.data[2].quantity, order.data[2].price)
+									order.response = response.data
+								} else if (order.response.symbol.includes('BTC')) {
+									console.log('step 3')
+									const response = await this.#newOrder(order.data[3].symbol, 'SELL', order.data[3].quantity, order.data[3].price)
+									order.response = response.data;
+								}
+							}
+							break;
+						default:
+							console.log(json);
+
 						//console.log(order.data);
 					}
 					// if(json.currentOrder == "PARTIALLY_FILLED")
